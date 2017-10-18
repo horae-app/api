7c180266897e565c8bc3946d75be954ee417ae3a
otherpirate <mauro_murari@hotmail.com>
2017-10-18 17:47:02 -0200

    Add route to confirm/cancel calendar

--- main.go
+++ main.go
@@ -36,6 +36,8 @@ func main() {
 	router.HandleFunc("/{companyId}/calendar/{calendarId}", Middlewares(Calendar.Delete)).Methods("DELETE")
 	router.HandleFunc("/{companyId}/calendar/{calendarId}/notify", Middlewares(Calendar.Notify)).Methods("GET")
 	router.HandleFunc("/{companyId}/calendar", Middlewares(Calendar.List)).Methods("GET")
+	router.HandleFunc("/{companyId}/calendar/{calendarId}/confirm", Middlewares(Calendar.Confirm)).Methods("GET")
+	router.HandleFunc("/{companyId}/calendar/{calendarId}/cancel", Middlewares(Calendar.Cancel)).Methods("GET")
 
 	router.HandleFunc("/contact/auth", Middlewares(Contact.UserAuth)).Methods("POST")
 	router.HandleFunc("/contact/calendar", Middlewares(Contact.Calendar)).Methods("POST")
